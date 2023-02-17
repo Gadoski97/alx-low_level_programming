f0b7a0d830e376135f12a07803acf4904c4bf8e7
Abolo Gad <Gadabolo007@gmail.com>
2023-02-17 14:28:07 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function without using print function
+ * Return: 0 (Genius)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
